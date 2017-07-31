1cdcea07ff613ac969aca71c354e06919c059410
sandy <sandyhysbz@gmail.com>
2017-07-31 08:55:09 +0800

    js库全部使用min文件，减少加载时间，调整等待时间至25s

--- app/main.js
+++ app/main.js
@@ -2,18 +2,18 @@
 
 require.config({
     paths: {
-        'angular': 'bower_components/angular/angular',
-        'angularAMD': 'bower_components/angularAMD/angularAMD',
-        'angular-ui-router': 'bower_components/angular-ui-router/release/angular-ui-router',
-        'showDown': 'bower_components/showdown/compressed/Showdown',
-        'angular-sanitize': 'bower_components/angular-sanitize/angular-sanitize',
+        'angular': 'bower_components/angular/angular.min.js',
+        'angularAMD': 'bower_components/angularAMD/angularAMD.min.js',
+        'angular-ui-router': 'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+        'showDown': 'bower_components/showdown/compressed/Showdown.min.js',
+        'angular-sanitize': 'bower_components/angular-sanitize/angular-sanitize.min.js',
         'angular-markdown': 'bower_components/angular-markdown-directive/markdown',
         'angular-markdown-editor': 'bower_components/angular-markdown-editor-ghiscoding/src/angular-markdown-editor',
         'bootstrap': 'bower_components/bootstrap/dist/js/bootstrap.min',
-        'bootstrap-markdown': 'bower_components/bootstrap-markdown/js/bootstrap-markdown',
+        'bootstrap-markdown': 'bower_components/bootstrap-markdown/js/bootstrap-markdown.js',
         'marked': 'bower_components/marked/marked.min',
         'angular-marked': 'bower_components/angular-marked/dist/angular-marked.min',
-        'hljs': 'bower_components/highlight.js.origin/src/highlight',
+        'hljs': 'bower_components/highlight.js.origin/src/highlight.js',
         'angular-highlight': 'bower_components/angular-highlightjs/angular-highlightjs.min',
         'jquery': 'bower_components/jquery/dist/jquery.min'
     },
@@ -46,5 +46,5 @@ require.config({
 
     },
     deps: ['app'],
-    waitSeconds: 15
+    waitSeconds: 25
 });
